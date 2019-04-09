917a5b2ac7b986d98850838671a3c1722127db5f
JL Peyret <jpeyret@gmail.com>
2019-04-08 17:09:16 -0700

    black d

--- tests/test_pip_stripper.py
+++ tests/test_pip_stripper.py
@@ -24,23 +24,31 @@ undefined = object()
 def build_args(**kwargs):
     pass
 
+
 import pdb
+
 
 def cpdb(**kwds):
     if cpdb.enabled == "once":
         cpdb.enabled = False
         return True
     return cpdb.enabled
+
+
 cpdb.enabled = False
+
 
 def rpdb(**kwds):
     return rpdb.enabled
+
+
 rpdb.enabled = False
 
 
 dn_test = os.path.dirname(__file__)
 
 from pip_stripper._baseutils import set_cpdb, ppp, debugObject, set_rpdb
+
 
 class Base(unittest.TestCase):
     testdir = None
@@ -58,10 +66,10 @@ class Base(unittest.TestCase):
         self.parser = Main.getOptParser()
         self.stderr = ""
 
-
     def tearDown(self):
         """Tear down test fixtures, if any."""
         os.chdir(self.oldpwd)
+
 
 class Test_Bad_options(Base):
     """Tests for `pip_stripper` package."""
@@ -72,13 +80,13 @@ class Test_Bad_options(Base):
 
         try:
             with self.assertRaises(SystemExit):
-                stderr = sys.stderr 
+                stderr = sys.stderr
                 sys.stderr = self
                 options = self.parser.parse_args(["-x"])
             sys.stderr = stderr
             self.assertTrue("unrecognized" in self.stderr)
-        except (Exception,SystemExit) as e:
-            if cpdb(): 
+        except (Exception, SystemExit) as e:
+            if cpdb():
                 pdb.set_trace()
             raise
 
@@ -92,7 +100,8 @@ class Test_Bad_options(Base):
         except (ValueError,) as e:
             self.assertTrue("configuration file" in str(e))
         except (Exception,) as e:
-            if cpdb(): pdb.set_trace()
+            if cpdb():
+                pdb.set_trace()
             raise
 
     def test_002_badconfig(self):
@@ -103,7 +112,8 @@ class Test_Bad_options(Base):
         except (IOError,) as e:
             pass
         except (Exception,) as e:
-            if cpdb(): pdb.set_trace()
+            if cpdb():
+                pdb.set_trace()
             raise
 
 
@@ -116,7 +126,7 @@ class WriterMixin(object):
     @property
     def testdir(self):
         if self._testdir is None:
-            prefix = self.baseprefix + (".%s" % (self.prefix) if self.prefix else "" )
+            prefix = self.baseprefix + (".%s" % (self.prefix) if self.prefix else "")
             self._testdir = tempfile.mkdtemp(prefix=prefix)
 
         return self._testdir
@@ -135,16 +145,16 @@ class WriterMixin(object):
             return dn_tgt
 
         except (Exception,) as e:
-            if cpdb(): pdb.set_trace()
+            if cpdb():
+                pdb.set_trace()
             raise
 
 
 class TestPip_Init(WriterMixin, Base):
-
     def test_001_init(self):
         try:
             print("self.testdir:%s" % (self.testdir))
-            options = self.parser.parse_args(["--init"])       
+            options = self.parser.parse_args(["--init"])
             mgr = Main(options)
 
             fnp = os.path.join(mgr.workdir, mgr.FN_CONFIG)
@@ -153,8 +163,10 @@ class TestPip_Init(WriterMixin, Base):
             ppp(config)
 
         except (Exception,) as e:
-            if cpdb(): pdb.set_trace()
+            if cpdb():
+                pdb.set_trace()
             raise
+
 
 class TestPip_Scan(WriterMixin, Base):
 
@@ -168,26 +180,28 @@ class TestPip_Scan(WriterMixin, Base):
 
     def test_001_scan(self):
         try:
-            options = self.parser.parse_args(["--init", "--workdir", self.workdir   ])       
+            options = self.parser.parse_args(["--init", "--workdir", self.workdir])
             mgr = Main(options)
-            if rpdb(): pdb.set_trace()
+            if rpdb():
+                pdb.set_trace()
             mgr.process()
 
         except (Exception,) as e:
-            if cpdb(): pdb.set_trace()
+            if cpdb():
+                pdb.set_trace()
             raise
 
 
 class TestMatchingBase(unittest.TestCase):
 
-    li_pip = ["cx-Oracle", "requests","python-dateutil","xxx"]
-    li_imp = ["dateutil","cx_Oracle","requests","yyy"]
+    li_pip = ["cx-Oracle", "requests", "python-dateutil", "xxx"]
+    li_imp = ["dateutil", "cx_Oracle", "requests", "yyy"]
 
     exp = {
-        "cx-Oracle" : "cx_Oracle",
-        "requests" : "requests",
-        "python-dateutil" : "dateutil"
-        }
+        "cx-Oracle": "cx_Oracle",
+        "requests": "requests",
+        "python-dateutil": "dateutil",
+    }
 
     def __repr__(self):
         return self.__class__.__name__
@@ -208,24 +222,20 @@ class TestMatchingBase(unittest.TestCase):
             ppp(self.matcher, "%s.matcher" % (self))
             ppp(self.matcher.pip, "pip")
             ppp(self.matcher.imp, "imp")
-            if cpdb(): 
+            if cpdb():
                 pdb.set_trace()
             raise
+
 
 class TestMatchingJinja(TestMatchingBase):
 
     li_pip = ["Jinja2"]
     li_imp = ["jinja2"]
 
-    exp = {
-        "Jinja2" : "jinja2"
-        }
+    exp = {"Jinja2": "jinja2"}
 
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # pdb.set_trace()
     set_cpdb(cpdb, remove=True)
     set_rpdb(rpdb, remove=True)
