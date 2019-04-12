5018d1788e4cbdcfa06c2b6e44646a311b238e03
JL Peyret <jpeyret@gmail.com>
2019-04-11 23:54:07 -0700

    added yaml reqss

--- setup.py
+++ setup.py
@@ -5,46 +5,47 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="JL Peyret",
-    author_email='jpeyret@gmail.com',
+    author_email="jpeyret@gmail.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="strip out unnecessary packages from requirementts",
-    install_requires=requirements,
+    # install_requires=requirements,
+    install_requires=["PyYAML"],
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='pip_stripper',
-    name='pip_stripper',
-    packages=find_packages(include=['pip_stripper']),
+    keywords="pip_stripper",
+    name="pip_stripper",
+    packages=find_packages(include=["pip_stripper"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/jpeyret/pip_stripper',
-    version='0.1.0',
+    url="https://github.com/jpeyret/pip_stripper",
+    version="0.1.0",
     zip_safe=False,
 )
