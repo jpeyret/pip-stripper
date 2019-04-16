312195b3f5e8e9aec6ae84d4a7fee9269faae305
JL Peyret <jpeyret@gmail.com>
2019-04-16 12:46:02 -0700

    Bump version: 0.1.8 → 0.2.0

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/jpeyret/pip-stripper",
-    version="0.1.8",
+    version="version='0.2.0'",
     zip_safe=False,
 )
